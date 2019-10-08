17c2addb7d6511463594fb01fcdb38fda0935e18
edoardo <phate09@hotmail.it>
2019-10-08 14:39:31 +0100

    optimised imports

--- verification_runs/cartpole_bab_runner.py
+++ verification_runs/cartpole_bab_runner.py
@@ -1,14 +1,5 @@
-import math
-
 import jsonpickle
-import numpy as np
-import torch.nn
 
-from dqn.dqn_agent import Agent
-from models.model_critic_sequential import QNetwork
-from plnn.bab_explore import DomainExplorer
-from plnn.branch_and_bound import bab
-from plnn.verification_network import VerificationNetwork
 from verification_runs.cartpole_bab_load import generateCartpoleDomainExplorer
 
 explorer, verification_model = generateCartpoleDomainExplorer()
@@ -17,6 +8,7 @@ explorer.explore(verification_model)
 frozen_safe = jsonpickle.encode([i.cpu().numpy() for i in explorer.safe_domains])
 frozen_unsafe = jsonpickle.encode([i.cpu().numpy() for i in explorer.unsafe_domains])
 frozen_ignore = jsonpickle.encode([i.cpu().numpy() for i in explorer.ignore_domains])
+
 with open("../runs/safe_domains.json", 'w+') as f:
     f.write(frozen_safe)
 with open("../runs/unsafe_domains.json", 'w+') as f:
