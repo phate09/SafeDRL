added test for domain analysis precision bug

mosaic/utils.py
@@ -14,23 +14,6 @@ def array_to_tuple(array: np.ndarray) -> Tuple[Tuple[float, float]]:
     return tuple_of_tuples
 
 
-def truncate(number: float, decimal_points: int):
-    decimal.getcontext().rounding = decimal.ROUND_DOWN
-    c = decimal.Decimal(number)
-    return float(round(c, decimal_points))
-
-
-def truncate_multi(interval: Tuple[float], decimal_points: int):
-    """Truncate the number of digits to a given number of decimal points
-    not working at the moment"""
-    return tuple([(interval[dimension][0], interval[dimension][1]) for dimension in range(len(interval))])
-
-
-def custom_rounding(x, prec=3, base=.05):
-    """Rounding with custom base"""
-    return round(base * round(float(x) / base), prec)
-
-
 def area_tensor(domain: torch.Tensor) -> float:
     '''
     Compute the area of the domain
@@ -133,7 +116,12 @@ def round_tuples(intervals: List[Tuple[Tuple[Tuple[float, float]], bool]], round
 
 
 def round_tuple(interval: Tuple[Tuple[float, float]], rounding: int) -> Tuple[Tuple[float, float]]:
-    return tuple([(float(round(x[0], rounding)), float(round(x[1], rounding))) for x in interval])
+    return interval  # tuple([(float(round(x[0], rounding)), float(round(x[1], rounding))) for x in interval])
+
+
+def open_close_tuple(interval: Tuple[Tuple[float, float]]):
+    eps = 1e-9
+    return tuple([(x[0], x[1] - eps) for x in interval])
 
 
 def inflate(current_interval: Tuple[Tuple[float, float]], rounding: int, eps=1e-6, ) -> Tuple[Tuple[float, float]]:

mosaic/workers/AbstractStepWorker.py
@@ -49,7 +49,7 @@ class AbstractStepWorker:
 def step_state(state: Tuple[Tuple[float, float]], action, env, rounding: int) -> Tuple[Tuple[Tuple], bool]:
     # given a state and an action, calculate next state
     env.reset()
-    state = round_tuple(state, rounding)  # round the state
+    # state = round_tuple(state, rounding)  # round the state
     env.state = tuple([iv.mpf([x[0], x[1]]) for x in state])
     next_state, reward, done, _ = env.step(action)
     return interval_unwrap(next_state, rounding), done
@@ -57,5 +57,6 @@ def step_state(state: Tuple[Tuple[float, float]], action, env, rounding: int) ->
 
 def interval_unwrap(state: np.ndarray, rounding: int) -> Tuple[Tuple[float, float]]:
     """From array of intervals to tuple of floats"""
-    unwrapped_state = tuple([(round(float(x.a), rounding), round(float(x.b), rounding)) for x in state])
+    # unwrapped_state = tuple([(round(float(x.a), rounding), round(float(x.b), rounding)) for x in state])
+    unwrapped_state = tuple([(float(x.a), float(x.b)) for x in state])
     return unwrapped_state

plnn/bab_explore.py
@@ -152,7 +152,8 @@ class DomainExplorer:
         mean_dom = torch.mean(normed_domain, dim=1)
         results = []
         for i, value in enumerate(mean_dom):
-            result = mosaic.utils.custom_rounding(value.item(), 3, precisions[i])
+            # result = mosaic.utils.custom_rounding(value.item(), 3, precisions[i])
+            result = float(value.item())
             results.append(result)
         return torch.tensor(results)
 
@@ -224,7 +225,7 @@ class DomainExplorer:
         dim = dim.item()
 
         # Now split over dimension dim:
-        half_length = round(edgelength / 2, rounding)
+        half_length = edgelength / 2
 
         # dom1: Upper bound in the 'dim'th dimension is now at halfway point.
         dom1 = domain.clone()
@@ -244,7 +245,7 @@ class DomainExplorer:
         diff = domain_array[:, 1] - domain_array[:, 0]
         edgelength = np.max(diff, 0).item()
         dim = np.argmax(diff, 0).item()
-        half_length = round(edgelength / 2, rounding)
+        half_length = edgelength / 2
         dom1 = domain_array.copy()
         dom1[dim, 1] -= half_length
         dom2 = domain_array.copy()