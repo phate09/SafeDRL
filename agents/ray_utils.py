93d544dab1751c8203ef56c90df1441120d5f424
edoardo <phate09@hotmail.it>
2020-09-18 16:36:02 +0100

    added utility method to load ray ppo agent from disk

---
+++ agents/ray_utils.py
@@ -0,0 +1,34 @@
+import ray
+from ray.rllib.agents.ppo import PPOTorchPolicy, ppo
+import torch
+
+from environment.pendulum_abstract import PendulumEnv
+
+
+def convert_ray_policy_to_sequential(policy: PPOTorchPolicy) -> torch.nn.Sequential:
+    layers_list = []
+    for seq_layer in policy.model._modules['_hidden_layers']:
+        for layer in seq_layer._modules['_model']:
+            print(layer)
+            layers_list.append(layer)
+    for layer in policy.model._modules['_logits']._modules['_model']:
+        print(layer)
+        layers_list.append(layer)
+    sequential_nn = torch.nn.Sequential(*layers_list)
+    return sequential_nn
+
+
+def load_sequential_from_ray(filename: str):
+    trainer = get_pendulum_ppo_agent()
+    trainer.restore(filename)
+    return convert_ray_policy_to_sequential(trainer.get_policy())
+
+
+def get_pendulum_ppo_agent():
+    config = {"env": PendulumEnv,  #
+              "model": {"fcnet_hiddens": [64, 64], "fcnet_activation": "relu"},  # model config
+              "vf_share_layers": False,  # try different lrs
+              "num_workers": 8,  # parallelism
+              "num_envs_per_worker": 5, "train_batch_size": 2000, "framework": "torch", "horizon": 1000}  # "batch_mode":"complete_episodes"
+    trainer = ppo.PPOTrainer(config=config)
+    return trainer
--- agents/train/load_PPO.py
+++ agents/train/load_PPO.py
@@ -3,30 +3,25 @@ from ray.rllib.agents.ppo import PPOTorchPolicy
 import torch.nn
 from environment.pendulum_abstract import PendulumEnv
 import ray
+from agents.ray_utils import convert_ray_policy_to_sequential, load_sequential_from_ray, get_pendulum_ppo_agent
 
-config = {"env": PendulumEnv,  # or "corridor" if registered above "env_config": {"corridor_length": 5, },"custom_model": "my_model"
-          "model": {"fcnet_hiddens": [64, 64], "fcnet_activation": "relu"},  # model config
-          "vf_share_layers": False,  # try different lrs
-          "num_workers": 8,  # parallelism
-          "num_envs_per_worker": 5, "train_batch_size": 2000, "framework": "torch", "horizon": 1000}  # "batch_mode":"complete_episodes"
+# config = {"env": PendulumEnv,  # or "corridor" if registered above "env_config": {"corridor_length": 5, },"custom_model": "my_model"
+#           "model": {"fcnet_hiddens": [64, 64], "fcnet_activation": "relu"},  # model config
+#           "vf_share_layers": False,  # try different lrs
+#           "num_workers": 8,  # parallelism
+#           "num_envs_per_worker": 5, "train_batch_size": 2000, "framework": "torch", "horizon": 1000}  # "batch_mode":"complete_episodes"
 ray.init()
-trainer = ppo.PPOTrainer(config=config)
+# trainer = ppo.PPOTrainer(config=config)
+# trainer.restore("/home/edoardo/ray_results/PPO_PendulumEnv_2020-09-18_11-23-17wpwqe3zd/checkpoint_25/checkpoint-25")
+# policy: PPOTorchPolicy = trainer.get_policy()
+trainer = get_pendulum_ppo_agent()
 trainer.restore("/home/edoardo/ray_results/PPO_PendulumEnv_2020-09-18_11-23-17wpwqe3zd/checkpoint_25/checkpoint-25")
-policy: PPOTorchPolicy = trainer.get_policy()
-layers_list = []
-for seq_layer in policy.model._modules['_hidden_layers']:
-    for layer in seq_layer._modules['_model']:
-        print(layer)
-        layers_list.append(layer)
-for layer in policy.model._modules['_logits']._modules['_model']:
-    print(layer)
-    layers_list.append(layer)
-sequential_nn = torch.nn.Sequential(*layers_list)
+policy = trainer.get_policy()
+sequential_nn = load_sequential_from_ray("/home/edoardo/ray_results/PPO_PendulumEnv_2020-09-18_11-23-17wpwqe3zd/checkpoint_25/checkpoint-25")
 for i in range(1000):
     test_input = torch.randn(2)
     seq_output = sequential_nn(test_input)
-    # trainer_output = trainer.compute_action(test_input.numpy())
-    # policy.model({"obs_flat":test_input})
     trainer_output = policy.model._logits(policy.model._hidden_layers(test_input))
-    assert torch.all(torch.eq(seq_output,trainer_output)).item()
+    assert torch.all(torch.eq(seq_output, trainer_output)).item()
 ray.shutdown()
+print("all good")
