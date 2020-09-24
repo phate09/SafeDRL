2640f7303a31d51d4134abe1806b576113f3acc6
edoardo <phate09@hotmail.it>
2020-09-25 00:25:35 +0100

    added plot for ppo agent

--- runnables/symbolic/ppo/domain_analysis.py
+++ runnables/symbolic/ppo/domain_analysis.py
@@ -3,75 +3,68 @@ import os
 import time
 import gym
 import ray
+import torch
+import numpy as np
+from sklearn.model_selection import ParameterGrid
+
 import mosaic.hyperrectangle_serialisation as serialisation
 import mosaic.utils as utils
 import prism.state_storage
 import symbolic.unroll_methods as unroll_methods
 import utility.domain_explorers_load
+from agents.dqn.dqn_sequential import TestNetwork, TestNetwork2
+from agents.ray_utils import load_sequential_from_ray
 from mosaic.hyperrectangle import HyperRectangle_action, HyperRectangle
+from mosaic.interval import Interval
+from plnn.verification_network_sym import SymVerificationNetwork
 from prism.shared_rtree import SharedRtree
+from symbolic.symbolic_interval import Symbolic_interval, Interval_network
 
-gym.logger.set_level(40)
-os.chdir(os.path.expanduser("~/Development") + "/SafeDRL")
+#
+# gym.logger.set_level(40)
+# os.chdir(os.path.expanduser("~/Development") + "/SafeDRL")
 local_mode = False
-allow_compute = True
-allow_save = False
-allow_load = False
+# allow_compute = True
+# allow_save = False
+# allow_load = False
 if not ray.is_initialized():
     ray.init(local_mode=local_mode, include_dashboard=True, log_to_driver=False)
-serialisation.register_serialisers()
-n_workers = int(ray.cluster_resources()["CPU"]) if not local_mode else 1
-storage = prism.state_storage.StateStorage()
-storage.reset()
+# serialisation.register_serialisers()
+# n_workers = int(ray.cluster_resources()["CPU"]) if not local_mode else 1
+# storage = prism.state_storage.StateStorage()
+# storage.reset()
 rounding = 2
 precision = 10 ** (-rounding)
 explorer, verification_model, env, current_interval, state_size, env_class = utility.domain_explorers_load.generatePendulumDomainExplorerPPO(precision, rounding, sym=True)
-print(f"Building the tree")
-rtree = SharedRtree()
-rtree.reset(state_size)
-# rtree.load_from_file(f"/home/edoardo/Development/SafeDRL/save/union_states_total_e{rounding}.p", rounding)
-print(f"Finished building the tree")
-# current_interval = HyperRectangle.from_tuple(tuple([(-0.72, -0.71), (-1, -0.9)]))
-# current_interval = HyperRectangle.from_tuple(tuple([(0.61, 0.62), (-1, -0.99)]))
-current_interval = current_interval.round(rounding)
-remainings = [current_interval]
-root = HyperRectangle_action.from_hyperrectangle(current_interval, None)
-storage.root = root
-storage.graph.add_node(storage.root)
-horizon = 4
-t = 0
-# %%
-if allow_load:
-    storage.load_state(f"/home/edoardo/Development/SafeDRL/save/nx_graph_e{rounding}.p")
-    rtree.load_from_file(f"/home/edoardo/Development/SafeDRL/save/union_states_total_e{rounding}.p", rounding)
-# %%
-if allow_compute:
-    iterations = 0
-    time_from_last_save = time.time()
-    while True:
-        print(f"Iteration {iterations}")
-        split_performed = unroll_methods.probability_iteration(storage, rtree, precision, rounding, env_class, n_workers, explorer, verification_model, state_size, horizon=horizon,
-                                                               allow_assign_actions=True, allow_refine=True)
-        if time.time() - time_from_last_save >= 60 * 5 and allow_save:
-            storage.save_state(f"/home/edoardo/Development/SafeDRL/save/nx_graph_e{rounding}.p")
-            rtree.save_to_file(f"/home/edoardo/Development/SafeDRL/save/union_states_total_e{rounding}.p")
-            print("Graph Saved - Checkpoint")
-            time_from_last_save = time.time()
-        if not split_performed or iterations == 50:
-            # utils.save_graph_as_dot(storage.graph)
-            if not split_performed:
-                print("No more split performed")
-            break
-        iterations += 1
-# %%
-if allow_save:
-    storage.save_state(f"/home/edoardo/Development/SafeDRL/save/nx_graph_e{rounding}.p")
-    rtree.save_to_file(f"/home/edoardo/Development/SafeDRL/save/union_states_total_e{rounding}.p")
-# %%
-# unroll_methods.remove_spurious_nodes(storage.graph)
-# storage.remove_unreachable()
-# storage.recreate_prism()
-# utils.save_graph_as_dot(storage.graph)
-storage.recreate_prism(horizon * 2)
-utils.show_heatmap(unroll_methods.get_property_at_timestep(storage, 1, ["lb"]),rounding=2,title=f"LB abstract horizon:{horizon}")
-utils.show_heatmap(unroll_methods.get_property_at_timestep(storage, 1, ["ub"]),rounding=2,title=f"UB abstract horizon:{horizon}")
+param_grid = {'param1': list(np.arange(-0.79, 0.79, precision).round(rounding)), 'param2': list(np.arange(-1, 1, precision).round(rounding))}
+grid = ParameterGrid(param_grid)
+sequential_nn = load_sequential_from_ray(os.path.expanduser("~/Development") + "/SafeDRL/save/PPO_PendulumEnv_2020-09-18_11-23-17wpwqe3zd/checkpoint_25/checkpoint-25")
+# net = TestNetwork2()
+sequential_nn.add_module("softmax", torch.nn.Softmax())
+# verif = SymVerificationNetwork(net.sequential)
+current_intervals = []
+for params in grid:
+    state = np.array((params['param1'], params['param2']))
+    current_intervals.append(torch.tensor(state))
+tensor_intervals = torch.stack(current_intervals)
+# ix = Symbolic_interval(lower=torch.tensor([[0, 0]], dtype=torch.float64, requires_grad=False), upper=torch.tensor([[0.01, 0.01]], dtype=torch.float64, requires_grad=False))
+ix2 = Symbolic_interval(lower=tensor_intervals, upper=tensor_intervals + precision)
+# verif.get_boundaries(ix, 0)
+inet = Interval_network(sequential_nn.double(), None)
+result_interval = inet(ix2)
+upper_bound = result_interval.u
+lower_bound = result_interval.l
+upper_bound_rectangles = []
+for x,y in zip(tensor_intervals,upper_bound[:,0]):
+    x_numpy = x.numpy()
+    x_numpy = np.stack([x_numpy,x_numpy+precision])
+    from_numpy = HyperRectangle.from_numpy(x_numpy)
+    upper_bound_rectangles.append((from_numpy,y.item()))
+lower_bound_rectangles = []
+for x, y in zip(tensor_intervals, lower_bound[:, 0]):
+    x_numpy = x.numpy()
+    x_numpy = np.stack([x_numpy, x_numpy + precision])
+    from_numpy = HyperRectangle.from_numpy(x_numpy)
+    lower_bound_rectangles.append((from_numpy, y.item()))
+utils.show_heatmap(upper_bound_rectangles,title="Upper bound probability of action=0",rounding=3)
+utils.show_heatmap(lower_bound_rectangles,title="Lower bound probability of action=0",rounding=3)
--- symbolic/symbolic_interval/symbolic_network.py
+++ symbolic/symbolic_interval/symbolic_network.py
@@ -554,6 +554,145 @@ class Interval_ReLU(nn.Module):
             return ix
 
 
+class Interval_Softmax(nn.Module):
+    def __init__(self, layer):
+        nn.Module.__init__(self)
+        self.layer = layer
+
+    def forward(self, ix):
+        # print(ix.u)
+        # print(ix.l)
+        if (isinstance(ix, Symbolic_interval)):
+
+            lower = ix.u.detach().clone().unsqueeze(1).repeat((1, ix.input_size, 1))
+            upper = ix.l.detach().clone().unsqueeze(1).repeat((1, ix.input_size, 1))
+            lower = lower - lower * torch.eye(ix.input_size)  # remove elements across diagonal
+            lower = lower + ix.l.detach().clone().unsqueeze(1).repeat((1, ix.input_size, 1)) * torch.eye(ix.input_size) # for each element add worst case
+            upper = upper - upper * torch.eye(ix.input_size)  # remove elements across diagonal
+            upper = upper + ix.u.detach().clone().unsqueeze(1).repeat((1, ix.input_size, 1)) * torch.eye(ix.input_size)
+            lower_softmax = torch.nn.functional.softmax(lower,2)
+            upper_softmax = torch.nn.functional.softmax(upper,2)
+            lower_masked = lower_softmax*torch.eye(ix.input_size)
+            upper_masked = upper_softmax*torch.eye(ix.input_size)
+            lower_result = torch.sum(lower_masked,dim=1)
+            upper_result = torch.sum(upper_masked,dim=1)
+            ix.update_lu(lower_result, upper_result)
+            return ix
+
+        if (isinstance(ix, Symbolic_interval_proj1)):
+            raise NotImplementedError
+            lower = ix.l
+            upper = ix.u
+            # print("sym u", upper)
+            # print("sym l", lower)
+
+            appr_condition = ((lower < 0) * (upper > 0)).detach()
+            mask = (lower > 0).type_as(lower)
+            mask[appr_condition] = upper[appr_condition] / (upper[appr_condition] - lower[appr_condition]).detach()
+
+            m = int(appr_condition.sum().item())
+            appr_ind = appr_condition.view(-1, ix.n).nonzero()
+
+            appr_err = mask * (-lower) / 2.0
+
+            if (m != 0):
+
+                if (ix.use_cuda):
+                    error_row = torch.zeros((m, ix.n), device=lower.get_device())
+                else:
+                    error_row = torch.zeros((m, ix.n))
+
+                error_row = error_row.scatter_(1, appr_ind[:, 1, None], appr_err[appr_condition][:, None])
+
+                edep_ind = lower.new(appr_ind.size(0), lower.size(0)).zero_()
+                edep_ind = edep_ind.scatter_(1, appr_ind[:, 0][:, None], 1)
+
+            ix.c = ix.c * mask + appr_err * appr_condition.type_as(lower)
+
+            for i in range(len(ix.edep)):
+                ix.edep[i] = ix.edep[i] * ix.edep_ind[i].mm(mask)
+
+            ix.idep = ix.idep * mask.view(ix.batch_size, 1, ix.n)
+            ix.idep_proj = ix.idep_proj * mask.view(ix.batch_size, ix.n)
+
+            if (m != 0):
+                ix.edep = ix.edep + [error_row]
+                ix.edep_ind = ix.edep_ind + [edep_ind]
+
+            return ix
+
+        if (isinstance(ix, Symbolic_interval_proj2)):
+            raise NotImplementedError
+            lower = ix.l
+            upper = ix.u
+            # print("sym u", upper)
+            # print("sym l", lower)
+            appr_condition = ((lower < 0) * (upper > 0)).detach()
+            mask = (lower > 0).type_as(lower)
+            m = int(appr_condition.sum().item())
+            # appr_ind = appr_condition.view(-1,ix.n).nonzero()
+
+            appr_err = upper / 2.0
+            appr_err = appr_err * (appr_condition.type_as(appr_err))
+
+            ix.edep = ix.edep * mask
+            ix.edep = ix.edep + appr_err
+
+            ix.c = ix.c * mask + appr_err
+
+            ix.idep = ix.idep * mask.view(ix.batch_size, 1, ix.n)
+            ix.idep_proj = ix.idep_proj * mask.view(ix.batch_size, ix.n)
+
+            return ix
+
+        if (isinstance(ix, Inverse_interval)):
+            raise NotImplementedError
+            lower = ix.l
+            upper = ix.u
+
+            # print("naive", upper)
+
+            if (ix.use_cuda):
+                appr_condition = ((lower < 0) * (upper > 0)).type(torch.Tensor).cuda(device=ix.c.get_device())
+            else:
+                appr_condition = ((lower < 0) * (upper > 0)).type(torch.Tensor)
+
+            mask = appr_condition * ((upper) / (upper - lower + 0.000001))
+            mask = mask + 1 - appr_condition
+            if (ix.use_cuda):
+                mask = mask * ((upper > 0).type(torch.Tensor).cuda(device=ix.c.get_device()))
+            else:
+                mask = mask * (upper > 0).type(torch.Tensor)
+            ix.mask.append(mask)
+            # print(ix.e.shape)
+            ix.update_lu(F.relu(ix.l), F.relu(ix.u))
+            return ix
+
+        if (isinstance(ix, Interval)):
+            '''
+            lower = ix.l.clamp(max=0)
+            upper = ix.u.clamp(min=0)
+            upper = torch.max(upper, lower + 1e-8)
+            mask = upper / (upper - lower)
+
+            ix.mask.append(mask)
+            '''
+            lower = ix.u.detach().clone().unsqueeze(1).repeat((1, ix.input_size, 1))
+            upper = ix.l.detach().clone().unsqueeze(1).repeat((1, ix.input_size, 1))
+            lower = lower - lower * torch.eye(ix.input_size)  # remove elements across diagonal
+            lower = lower + ix.l.detach().clone() * torch.eye(ix.input_size)  # for each element add worst case
+            upper = upper - upper * torch.eye(ix.input_size)  # remove elements across diagonal
+            upper = upper + ix.u.detach().clone() * torch.eye(ix.input_size)
+            lower_softmax = torch.nn.functional.softmax(lower, 2)
+            upper_softmax = torch.nn.functional.softmax(upper, 2)
+            lower_masked = lower_softmax * torch.eye(ix.input_size)
+            upper_masked = upper_softmax * torch.eye(ix.input_size)
+            lower_result = torch.sum(lower_masked, dim=1)
+            upper_result = torch.sum(upper_masked, dim=1)
+            ix.update_lu(lower_result, upper_result)
+            return ix
+
+
 class Interval_Flatten(nn.Module):
     def __init__(self):
         nn.Module.__init__(self)
