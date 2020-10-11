783883cc179a09864f6739504965df82164f338d
edoardo <phate09@hotmail.it>
2020-10-11 16:24:05 +0100

    good validation results and plotting

--- polyhedra/runnable/toy_2d/load_2d_classifier.py
+++ polyhedra/runnable/toy_2d/load_2d_classifier.py
@@ -8,8 +8,15 @@ from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from polyhedra.runnable.toy_2d.train_2d_classifier import Net
+import mosaic.utils as utils
 
 net = Net()
 net.load_state_dict(torch.load("model.pt"))
 net.eval()
-
+points = np.random.random((10000, 2)) * 5
+result = torch.argmax(net(torch.from_numpy(points).float()),dim=1)
+mask_positives = result.bool().detach().numpy()
+positives = points[mask_positives]
+negatives = points[np.invert(mask_positives)]
+fig = utils.scatter_plot(positives,negatives)
+fig.show()
--- polyhedra/runnable/toy_2d/train_2d_classifier.py
+++ polyhedra/runnable/toy_2d/train_2d_classifier.py
@@ -7,15 +7,17 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+import mosaic.utils as utils
 
 
 class ToyDataset(torch.utils.data.Dataset):
     """generates 2d points around -10 + 10 with true value between range -1 +1"""
 
-    def __init__(self, size=10000):
-        x = np.random.random((size, 2)) * 10 - 5
+    def __init__(self, size=10000, seed=0):
+        np.random.seed(seed=seed)
+        x = np.random.random((size, 2)) * 5
         self.x = torch.from_numpy(x).float()
-        y = np.array([1 if 1 > x[0] > -1 and 1 > x[1] > -1 else 0 for x in x])
+        y = np.array([1 if 2 > x[0] and 2 > x[1] else 0 for x in x])
         self.y = torch.from_numpy(y).long()
 
     def __getitem__(self, index):
@@ -48,7 +50,8 @@ class CustomTrainingOperator(TrainingOperator):
         model = Net()
 
         # Create optimizer.
-        optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
+        # optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
 
         # Create loss.
         loss = torch.nn.CrossEntropyLoss()
@@ -60,16 +63,26 @@ class CustomTrainingOperator(TrainingOperator):
         self.register_data(train_loader=train_loader, validation_loader=val_loader)
 
 
-ray.init(local_mode=True)
+if __name__ == '__main__':
+    ray.init(local_mode=False)
 
-trainer1 = TorchTrainer(training_operator_cls=CustomTrainingOperator, num_workers=1, use_gpu=False, config={"batch_size": 64})
+    trainer1 = TorchTrainer(training_operator_cls=CustomTrainingOperator, num_workers=4, use_gpu=False, config={"batch_size": 256})
 
-for i in range(10):
-    stats = trainer1.train()
-    print(stats)
-    val_stats = trainer1.validate()
-    print(val_stats)
-model = trainer1.get_model()
-torch.save(model.state_dict(), "model.pt")  # save the model in the current folder
-trainer1.shutdown()
-print("success!")
+    for i in range(200):
+        stats = trainer1.train()
+        print(stats)
+        val_stats = trainer1.validate()
+        print(val_stats)
+    net = trainer1.get_model()
+    # torch.save(net.state_dict(), "model.pt")  # save the model in the current folder
+    trainer1.shutdown()
+    print("success!")
+
+    points = np.random.random((10000, 2)) * 5
+    values = net(torch.from_numpy(points).float())
+    result = torch.argmax(values, dim=1)
+    mask_positives = result.bool().detach().numpy()
+    positives = points[mask_positives]
+    negatives = points[np.invert(mask_positives)]
+    fig = utils.scatter_plot(positives, negatives)
+    fig.show()
