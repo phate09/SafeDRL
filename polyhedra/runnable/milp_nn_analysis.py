8ac4dcdc8bb195167efcc7f5b92fb9eb71efe8cc
edoardo <phate09@hotmail.it>
2020-11-29 17:45:37 +0000

    named constranints added loop for n timesteps

--- polyhedra/runnable/milp_nn_analysis.py
+++ polyhedra/runnable/milp_nn_analysis.py
@@ -15,48 +15,48 @@ import itertools
 def generate_input_region(gurobi_model):
     input = gurobi_model.addVars(6, lb=float("-inf"), name="input")
     # x_lead
-    gurobi_model.addConstr(input[0] >= 90)
-    gurobi_model.addConstr(input[0] <= 92)
+    gurobi_model.addConstr(input[0] >= 90, name="input_constr_1")
+    gurobi_model.addConstr(input[0] <= 92, name="input_constr_2")
     # x_ego
-    gurobi_model.addConstr(input[1] >= 30)
-    gurobi_model.addConstr(input[1] <= 31)
+    gurobi_model.addConstr(input[1] >= 30, name="input_constr_3")
+    gurobi_model.addConstr(input[1] <= 31, name="input_constr_4")
     # v_lead
-    gurobi_model.addConstr(input[2] >= 20)
-    gurobi_model.addConstr(input[2] <= 30)
+    gurobi_model.addConstr(input[2] >= 20, name="input_constr_5")
+    gurobi_model.addConstr(input[2] <= 30, name="input_constr_6")
     # v_ego
-    gurobi_model.addConstr(input[3] >= 30)
-    gurobi_model.addConstr(input[3] <= 30.5)
+    gurobi_model.addConstr(input[3] >= 30, name="input_constr_7")
+    gurobi_model.addConstr(input[3] <= 30.5, name="input_constr_8")
     # y_lead
-    gurobi_model.addConstr(input[4] >= 0)
-    gurobi_model.addConstr(input[4] <= 0)
+    gurobi_model.addConstr(input[4] >= 0, name="input_constr_9")
+    gurobi_model.addConstr(input[4] <= 0, name="input_constr_10")
     # y_ego
-    gurobi_model.addConstr(input[5] >= 0)
-    gurobi_model.addConstr(input[5] <= 0)
+    gurobi_model.addConstr(input[5] >= 0, name="input_constr_11")
+    gurobi_model.addConstr(input[5] <= 0, name="input_constr_12")
     return input
 
 
-def generate_mock_guard(gurobi_model: grb.Model, input, action_ego=0):
+def generate_mock_guard(gurobi_model: grb.Model, input, action_ego=0, t=0):
     if action_ego == 0:
-        y = gurobi_model.addVars(2, vtype=grb.GRB.BINARY, name="or_indicator")
-        # gurobi_model.addConstr(grb.or_((input[0] - input[1]) >= 50, input[3] >= 25))
-        # gurobi_model.addConstr((y[0] == 1) >> ((input[0] - input[1]) >= 50))
-        gurobi_model.addGenConstrIndicator(y[0], True, ((input[0] - input[1]) >= 50))
-        # gurobi_model.addConstr((y[1] == 1) >> (input[3] >= 25))
-        gurobi_model.addGenConstrIndicator(y[1], True, (input[3] >= 30))
-        gurobi_model.addConstr(y[0] + y[1] == 1)
+        y = gurobi_model.addVars(2, vtype=grb.GRB.BINARY, name=f"or_indicator_{t}")
+        gurobi_model.addGenConstrIndicator(y[0], True, ((input[0] - input[1]) >= 50), name=f"cond1_{t}")
+        gurobi_model.addGenConstrIndicator(y[1], True, (input[3] >= 30), name=f"cond2_{t}")
+        gurobi_model.addConstr(y[0] + y[1] == 1, name=f"cond3_{t}")
     else:
-        gurobi_model.addConstr((input[0] - input[1]) <= 50)
-        gurobi_model.addConstr(input[3] <= 25)
+        constr3 = gurobi_model.getConstrByName(f"cond3_{t}")
+        if constr3 is not None:
+            gurobi_model.remove(constr3)
+        gurobi_model.addConstr((input[0] - input[1]) <= 50, name=f"cond1_{t}")
+        gurobi_model.addConstr(input[3] <= 25, name=f"cond2_{t}")
 
 
-def apply_dynamic(input, gurobi_model: grb.Model, action_ego=0):
+def apply_dynamic(input, gurobi_model: grb.Model, action_ego=0, t=0):
     x_lead = input[0]
     x_ego = input[1]
     v_lead = input[2]
     v_ego = input[3]
     y_lead = input[4]
     y_ego = input[5]
-    z = gurobi_model.addVars(6, lb=float("-inf"), name="x_prime")
+    z = gurobi_model.addVars(6, lb=float("-inf"), name=f"x_prime_{t}")
     a_ego = 1
     dt = .1
     if action_ego == 0:
@@ -70,12 +70,12 @@ def apply_dynamic(input, gurobi_model: grb.Model, action_ego=0):
     x_ego_prime = x_ego + (v_ego + (y_ego + acceleration) * dt) * dt
     # delta_x_prime = (x_lead + (v_lead + (y_lead + 0) * dt) * dt) - (x_ego + (v_ego + (y_ego + acceleration) * dt) * dt)
     # delta_v_prime = (v_lead + (y_lead + 0) * dt) - (v_ego + (y_ego + acceleration) * dt)
-    gurobi_model.addConstr(z[0] == x_lead_prime)
-    gurobi_model.addConstr(z[1] == x_ego_prime)
-    gurobi_model.addConstr(z[2] == v_lead_prime)
-    gurobi_model.addConstr(z[3] == v_ego_prime)
-    gurobi_model.addConstr(z[4] == y_lead)  # no change in y_lead
-    gurobi_model.addConstr(z[5] == y_ego_prime)
+    gurobi_model.addConstr(z[0] == x_lead_prime, name=f"dyna_constr_1_{t}")
+    gurobi_model.addConstr(z[1] == x_ego_prime, name=f"dyna_constr_2_{t}")
+    gurobi_model.addConstr(z[2] == v_lead_prime, name=f"dyna_constr_3_{t}")
+    gurobi_model.addConstr(z[3] == v_ego_prime, name=f"dyna_constr_4_{t}")
+    gurobi_model.addConstr(z[4] == y_lead, name=f"dyna_constr_5_{t}")  # no change in y_lead
+    gurobi_model.addConstr(z[5] == y_ego_prime, name=f"dyna_constr_6_{t}")
     return z
 
 
@@ -127,27 +127,47 @@ def main():
     input = generate_input_region(gurobi_model)
     x_results = optimise(template, gurobi_model, input)
     x_vertices = pypoman.duality.compute_polytope_vertices(template, x_results)
-    generate_mock_guard(gurobi_model, input, 0)
-    # apply dynamic
-    x_prime = apply_dynamic(input, gurobi_model)
-    # enclosing halfspaces  # max <template,y>
-    x_prime_results = optimise(template, gurobi_model, x_prime)  # dimensionality reduction  # plot
-    x_prime_vertices = pypoman.duality.compute_polytope_vertices(template, x_prime_results)
-    show_polygon_list([x_vertices,x_prime_vertices])
+    vertices_list = []
+    vertices_list.append(x_vertices)
+    for t in range(4):
+        generate_mock_guard(gurobi_model, input, 0, t=t)
+        # apply dynamic
+        x_prime = apply_dynamic(input, gurobi_model, 0, t=t)
+        # enclosing halfspaces  # max <template,y>
+        x_prime_results = optimise(template, gurobi_model, x_prime)  # h representation
+        x_prime_vertices = pypoman.duality.compute_polytope_vertices(template, x_prime_results)
+        vertices_list.append(x_prime_vertices)
+        input = x_prime
+    show_polygon_list(vertices_list)
+
+
+def transform_vertices(polygon_vertices_list):
+    result = []
+    for vertices in polygon_vertices_list:
+        transformed_vertices = []
+        for vertex in vertices:
+            transformed_vertex = np.zeros(shape=(2,))
+            transformed_vertex[0] = vertex[0] - vertex[1]
+            transformed_vertex[1] = vertex[2] - vertex[3]
+            transformed_vertices.append(transformed_vertex)
+        result.append(transformed_vertices)
+    return result
 
 
 def show_polygon_list(polygon_vertices_list):  # x_prime_vertices, x_vertices
 
-    scaler = StandardScaler()
-    scaler.fit(polygon_vertices_list[0])#list(itertools.chain.from_iterable(polygon_vertices_list)))
-    scaled_list = []
-    for x in polygon_vertices_list:
-        scaled_list.append(scaler.transform(x))
-    pca = PCA(n_components=2)
-    pca.fit(list(itertools.chain.from_iterable(scaled_list)))#
-    principal_components_list = []
-    for x_scaled in scaled_list:
-        principal_components_list.append(pca.transform(x_scaled))
+    # scaler = StandardScaler()
+    # scaler.fit(polygon_vertices_list[0])  # list(itertools.chain.from_iterable(polygon_vertices_list)))
+    # scaled_list = []
+    # for x in polygon_vertices_list:
+    #     scaled_list.append(scaler.transform(x))
+    # pca = PCA(n_components=2)
+    # pca.fit(list(itertools.chain.from_iterable(scaled_list)))  #
+    # principal_components_list = []
+    # for x_scaled in scaled_list:
+    #     principal_components_list.append(pca.transform(x_scaled))
+    # todo apply transformation of points
+    principal_components_list = transform_vertices(polygon_vertices_list)
     traces = []
     for principal_component in principal_components_list:
         traces.append(compute_polygon_trace(principal_component))
