fixed sign error in bouncing ball unsafe zone better stopper to the training of bouncing ball retrained bouncing ball with better parameters

agents/ppo/load_PPO_bouncingball.py
@@ -1,20 +1,33 @@
 import ray
 import torch.nn
 import numpy as np
+from ray.rllib.agents.ppo import ppo
+
 from agents.ppo.train_PPO_bouncingball import get_PPO_trainer
+from agents.ppo.tune.tune_train_PPO_bouncing_ball import get_PPO_config
 from agents.ray_utils import convert_ray_policy_to_sequential
 from environment.bouncing_ball_old import BouncingBall
 
 ray.init()
-config, trainer = get_PPO_trainer(use_gpu=0)
-trainer.restore("/home/edoardo/ray_results/PPO_BouncingBall_2021-01-04_18-58-32smp2ln1g/checkpoint_272/checkpoint-272")  # ~980 score
+# config, trainer = get_PPO_trainer(use_gpu=0)
+# trainer.restore("/home/edoardo/ray_results/PPO_BouncingBall_2021-01-04_18-58-32smp2ln1g/checkpoint_272/checkpoint-272")  # ~980 score
+# policy = trainer.get_policy()
+# sequential_nn = convert_ray_policy_to_sequential(policy).cpu()
+# layers = []
+# for l in sequential_nn:
+#     layers.append(l)
+#
+# sequential_nn2 = torch.nn.Sequential(*layers)
+ray.init(ignore_reinit_error=True)
+config = get_PPO_config(1234)
+trainer = ppo.PPOTrainer(config=config)
+trainer.restore("/home/edoardo/ray_results/tune_PPO_bouncing_ball/PPO_BouncingBall_71684_00004_4_2021-01-18_23-48-21/checkpoint_10/checkpoint-10")
 policy = trainer.get_policy()
 sequential_nn = convert_ray_policy_to_sequential(policy).cpu()
 layers = []
 for l in sequential_nn:
     layers.append(l)
-
-sequential_nn2 = torch.nn.Sequential(*layers)
+nn = torch.nn.Sequential(*layers)
 env = BouncingBall()
 state = env.reset()
 env.p = 7

agents/ppo/tune/tune_train_PPO_bouncing_ball.py
@@ -35,6 +35,13 @@ class TorchCustomModel(TorchModelV2, nn.Module):
         return torch.reshape(self.torch_sub_model.value_function(), [-1])
 
 
+def stopper(trial_id, result):
+    if "evaluation" in result:
+        return result["evaluation"]["episode_reward_min"] > 800 and result["evaluation"]["episode_len_mean"] == 1000
+    else:
+        return False
+
+
 def get_PPO_config(seed, use_gpu: float = 1):
     ModelCatalog.register_custom_model("my_model", TorchCustomModel)
     config = {"env": BouncingBall,  #
@@ -46,10 +53,10 @@ def get_PPO_config(seed, use_gpu: float = 1):
               "grad_clip": 300,
               # "clip_rewards": 5,
               "num_workers": 3,  # parallelism
-              "num_envs_per_worker": 2,
-              "batch_mode": "complete_episodes",
+              "num_envs_per_worker": 4,
+              "batch_mode": "truncate_episodes",
               "evaluation_interval": 10,
-              "evaluation_num_episodes": 20,
+              "evaluation_num_episodes": 30,
               "use_gae": True,  #
               "lambda": 0.95,  # gae lambda param
               "num_sgd_iter": 10,
@@ -78,14 +85,16 @@ if __name__ == "__main__":
     datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     tune.run(
         "PPO",
-        stop={"info/num_steps_trained": 5e6, "episode_reward_mean": 900},
+        stop=stopper,  # {"info/num_steps_trained": 5e6, "episode_reward_min": 850},
         config=config,
         name=f"tune_PPO_bouncing_ball",
         checkpoint_freq=10,
         checkpoint_at_end=True,
+        keep_checkpoints_num=5,
+        checkpoint_score_attr="episode_reward_mean",
         log_to_file=True,
         # resume="PROMPT",
         verbose=1,
-        num_samples=10
+        num_samples=5
     )
     ray.shutdown()

environment/bouncing_ball_old.py
@@ -14,11 +14,11 @@ class BouncingBall(gym.Env):
         self.dt = 0.1
         self.seed()
         if config is not None:
-            self.dt = config.get(["tau"], 0.1)
+            self.dt = config.get("tau", 0.1)
 
     def reset(self):
         self.p = 7 + self.np_random.uniform(0, 3)
-        self.v = 0
+        self.v = self.np_random.uniform(-1, 1)
         return np.array((self.p, self.v))
 
     def step(self, action):

polyhedra/runnable/experiment/run_experiment_bouncing_ball.py
@@ -26,10 +26,12 @@ class BouncingBallExperiment(Experiment):
         _, template = self.get_template(0)
         self.analysis_template: np.ndarray = template
         self.time_horizon = 500
+        self.rounding_value = 2 ** 8
         p = Experiment.e(self.env_input_size, 0)
         v = Experiment.e(self.env_input_size, 1)
-        self.unsafe_zone: List[Tuple] = [([p, v], np.array([0, 1]))]
+        self.unsafe_zone: List[Tuple] = [([p, -v, v], np.array([0, 1, 0]))]
         self.nn_path = "/home/edoardo/ray_results/tune_PPO_bouncing_ball/PPO_BouncingBall_c7326_00000_0_2021-01-16_05-43-36/checkpoint_36/checkpoint-36"
+        # self.nn_path = "/home/edoardo/ray_results/tune_PPO_bouncing_ball/PPO_BouncingBall_71684_00004_4_2021-01-18_23-48-21/checkpoint_10/checkpoint-10"
 
     @ray.remote
     def post_milp(self, x, nn, output_flag, t, template):
@@ -182,7 +184,7 @@ class BouncingBallExperiment(Experiment):
 
         return z
 
-    def plot(self,vertices_list, template, template_2d):
+    def plot(self, vertices_list, template, template_2d):
         self.generic_plot("v", "p", vertices_list, template, template_2d)
         # pass
 