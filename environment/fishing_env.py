fc63bde93f122713070f6116e813b2bb53539cc0
edoardo <phate09@hotmail.it>
2021-04-26 12:21:08 +0100

    added environments

---
+++ environment/fishing_env.py
@@ -0,0 +1,25 @@
+from gym import spaces
+
+# from gym_fishing.envs.base_fishing_env import BaseFishingEnv
+from environment.base_fishing_env import BaseFishingEnv
+
+
+class FishingEnv(BaseFishingEnv):
+    def __init__(
+        self,
+        r=0.3,
+        K=1,
+        sigma=0.0,
+        n_actions=100,
+        init_state=0.75,
+        Tmax=100,
+        file=None,
+    ):
+        super().__init__(
+            params={"r": r, "K": K, "sigma": sigma, "x0": init_state},
+            Tmax=Tmax,
+            file=file,
+        )
+        # override to use discrete actions
+        self.n_actions = n_actions
+        self.action_space = spaces.Discrete(n_actions)
