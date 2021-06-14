6b8f81dd929c5efcdbffa0982c4716190732dfc6
edoardo <phate09@hotmail.it>
2021-06-14 19:46:17 +0100

    added stopping_car_invariant_net.py

---
+++ agents/ppo/invariant_net/stopping_car_invariant_net.py
@@ -0,0 +1,20 @@
+import torch
+import numpy as np
+
+
+class StoppingCarInvariantNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.env_size = 6
+        weights = np.array([[1, -1, 0, 0, 0, 0]])  # x_lead-x_ego
+        linear = torch.nn.Linear(6, 1)
+        linear.weight.data = torch.tensor(weights).float()
+        self.net = torch.nn.Sequential(linear, )
+
+    def forward(self, input: torch.Tensor):
+        return self.net(input)
+
+
+if __name__ == '__main__':
+    nn = StoppingCarInvariantNet()
+    print(nn(torch.tensor([10, 1, 0, 0, 0, 0]).float()))
